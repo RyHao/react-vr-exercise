cba02d0bfb97959f1b6eaec39b1c7829a55b9868
RyHao <kinghao914@gmail.com>
2017-05-28 21:26:42 +0800

    feat(basics): Add models section

---
+++ Basics/sections/models/index.vr.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { AppRegistry, asset, Pano, View, Model } from 'react-vr';
+
+export default class Basics extends Component {
+  render() {
+    return (
+      <View>
+        <Pano source={asset('starry-sky.jpg')} />
+        <Model
+          source={{ obj: asset('Tree.obj') }}
+          style={{
+            transform: [
+              { translate: [0, -1, -3] }
+            ]
+          }}
+          texture={'http://i.imgur.com/14HMTcQb.jpg'}
+        />
+      </View>
+    );
+  }
+};
+
+AppRegistry.registerComponent('Basics', () => Basics);
